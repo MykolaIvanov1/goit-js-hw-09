9afc5a3dca9dec99f46c77d30f215d1f1815dc03
MykolaIvanov1 <111191656+MykolaIvanov1@users.noreply.github.com>
2023-04-21 19:09:57 +0000

    Deploying to gh-pages from @ MykolaIvanov1/goit-js-hw-09@539febda9985bf53ca388d45aa889cccddae7ebb 🚀

---
+++ 01-color-switcher.775ab28d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]"),a=null,n=t.disabled=!1;d.disabled=!0;function r(){return e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){if(n)return;a=setInterval(r,1e3),n=t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(a),n=t.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.775ab28d.js.map
